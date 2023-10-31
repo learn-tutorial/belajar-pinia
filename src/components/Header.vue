32443c64b3efd0f09ae0837324c0634c573b992b
aditia <superditif@gmail.com>
2023-11-01 06:52:36 +0700

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    ini header
+    <div>
+        state: {{ counter }}
+        <br />
+        getters: {{ getCount }}
+        <br />
+        getters: {{ doubleCount }}
+        <br />
+        getters: {{ divideByTwo }}
+        <br />
+        pokemon: {{ pokemon }}
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useCounterStore } from '../stores/counter'
+import { usePokemonStore } from '../stores/pokemon';
+
+const counteStore = useCounterStore()
+const pokemonStore = usePokemonStore()
+
+const counter = computed(() => counteStore.count)
+const getCount = computed(() => counteStore.getCount)
+const doubleCount = computed(() => counteStore.doubleCount)
+const divideByTwo = computed(() => counteStore.divideBy(2))
+
+const pokemon = computed(() => pokemonStore.pokemonDetails)
+
+counteStore.$subscribe((mutation, state) => {
+    console.log(mutation, state)
+})
+</script>
